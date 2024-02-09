342c4b0bea66921fdc2449ff29b8593d600791cb
Henrique1507 <124264857+Henrique1507@users.noreply.github.com>
2024-02-09 11:40:44 -0300

    Feat: adiciona estilização do body

--- src/components/layouts/Body.js
+++ src/components/layouts/Body.js
@@ -1,10 +1,19 @@
 import torcida from '../../img/spfc-torcida.jpg'
+import styles from '../../components/layouts/Body.module.css'
 
 function Body () {
+
+    function tricolor() {
+        return(
+            alert('Parabéns! Você é da Torcida Independente')
+        )
+    }
+    
+
     return (
-        <div>
-            <img src={torcida} alt='torcida'/>
-            <button>CLIQUE AQUI PARA FAZER PARTE DA TORCIDA</button>
+        <div className={styles.body}>
+            <div className={styles.torcida}><img src={torcida} alt='torcida'/></div>
+            <button className={styles.button} onClick={tricolor}>CLIQUE AQUI PARA FAZER PARTE DA TORCIDA</button>
         </div>
     )
 }

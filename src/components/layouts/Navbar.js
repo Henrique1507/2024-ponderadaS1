49a87f7cb922c026717f9b14aa21431909d83f32
Henrique1507 <124264857+Henrique1507@users.noreply.github.com>
2024-02-09 10:55:48 -0300

    Fix: arruma links das redes sociais

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -6,10 +6,10 @@ function Navbar () {
         <nav className={styles.navbar}>
             <div className={styles.spfc}><img src={spfc} alt="spfc"/></div>
             <ul className={styles.list}>
-                <li className={styles.sociais}><a href='https://www.instagram.com/?hl=pt-br'>Instagram</a></li>
-                <li className={styles.sociais}><a href='https://www.instagram.com/?hl=pt-br'>Youtube</a></li>
-                <li className={styles.sociais}><a href='https://www.instagram.com/?hl=pt-br'>Twitter</a></li>
-                <li className={styles.sociais}><a href='https://www.instagram.com/?hl=pt-br'>Especial</a></li>
+                <li className={styles.sociais}><a href='https://www.instagram.com/saopaulofc/?hl=pt-br'>Instagram</a></li>
+                <li className={styles.sociais}><a href='https://www.youtube.com/@saopaulofc'>Youtube</a></li>
+                <li className={styles.sociais}><a href='https://twitter.com/SaoPauloFC'>Twitter</a></li>
+                <li className={styles.sociais}><a href='https://www.youtube.com/watch?v=WU1Vum9cDbE&ab_channel=eduklotz'>Especial</a></li>
             </ul>
         </nav>
     )

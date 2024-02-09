a5fbd5bcae6bbb7d967f80f2302add39c648ca34
Henrique1507 <124264857+Henrique1507@users.noreply.github.com>
2024-02-09 11:26:22 -0300

    Feat: criação do body
    Adiciona imagem e botão

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import '../src/index.css'
 import Footer from './components/layouts/Footer';
 import Navbar from "./components/layouts/Navbar";
+import Body from './components/layouts/Body';
 
 function App() {
   return (
     <div>
       <Navbar/>
-      
+      <Body/>
       <Footer/>
     </div>
   );
---
+++ src/components/layouts/Body.js
@@ -0,0 +1,12 @@
+import torcida from '../../img/spfc-torcida.jpg'
+
+function Body () {
+    return (
+        <div>
+            <img src={torcida} alt='torcida'/>
+            <button>CLIQUE AQUI PARA FAZER PARTE DA TORCIDA</button>
+        </div>
+    )
+}
+
+export default Body

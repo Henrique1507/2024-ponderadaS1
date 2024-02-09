a25fc8c33d863da77d1d4d47e345e12cf7c5be25
Henrique1507 <124264857+Henrique1507@users.noreply.github.com>
2024-02-09 10:49:41 -0300

    Feat: adiciona estilização da navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import '../src/index.css'
 import Navbar from "./components/layouts/Navbar";
 
 function App() {
--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,14 +1,15 @@
 import spfc from '../../img/spfc.jpg'
+import styles from '../../components/layouts/Navbar.module.css'
 
 function Navbar () {
     return(
-        <nav>
-            <img src={spfc} alt="spfc"/>
-            <ul>
-                <li><a href='https://www.instagram.com/?hl=pt-br'>Instagram</a></li>
-                <li><a href='https://www.instagram.com/?hl=pt-br'>Youtube</a></li>
-                <li><a href='https://www.instagram.com/?hl=pt-br'>Twitter</a></li>
-                <li><a href='https://www.instagram.com/?hl=pt-br'>Especial</a></li>
+        <nav className={styles.navbar}>
+            <div className={styles.spfc}><img src={spfc} alt="spfc"/></div>
+            <ul className={styles.list}>
+                <li className={styles.sociais}><a href='https://www.instagram.com/?hl=pt-br'>Instagram</a></li>
+                <li className={styles.sociais}><a href='https://www.instagram.com/?hl=pt-br'>Youtube</a></li>
+                <li className={styles.sociais}><a href='https://www.instagram.com/?hl=pt-br'>Twitter</a></li>
+                <li className={styles.sociais}><a href='https://www.instagram.com/?hl=pt-br'>Especial</a></li>
             </ul>
         </nav>
     )
